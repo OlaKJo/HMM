0060b5f4d34f573cb3d5fe456793635e2bd093dc
Ola Johansson <ola.oj.johansson@gmail.com>
2018-02-28 22:01:06 +0100

    updated jar, now takes arguments for world size

--- src/control/Main.java
+++ src/control/Main.java
@@ -6,31 +6,44 @@ import view.RobotLocalizationViewer;
 
 public class Main {
 	/*
-	 * build your own if you like, this is just an example of how to start the viewer
-	 * ...
+	 * build your own if you like, this is just an example of how to start the
+	 * viewer ...
 	 */
-	
+
 	final static int ROWS = 8;
 	final static int COLS = 8;
 	final static int HEAD = 4;
-	
-	public static void main( String[] args) {
-				
-		Robot marvin = new Robot(ROWS, COLS, HEAD);
-		
+
+	public static void main(String[] args) {
+		int rows = ROWS;
+		int cols = COLS;
+
+		if (args.length >= 1) {
+			try {
+				int arg = Integer.parseInt(args[0]);
+				if (arg >= 5 && arg < 20) {
+					rows = Integer.parseInt(args[0]);
+					cols = rows;
+				}
+			} catch (Exception e) {
+				System.out.println("There was an error with the input arguments. " + e.toString());
+			}
+		}
+		Robot marvin = new Robot(rows, cols, HEAD);
+
 		/*
-		 * generate you own localiser / estimator wrapper here to plug it into the 
+		 * generate you own localiser / estimator wrapper here to plug it into the
 		 * graphics class.
 		 */
 
-		EstimatorInterface localizer = new Localizer(ROWS, COLS, HEAD, marvin);
+		EstimatorInterface localizer = new Localizer(rows, cols, HEAD, marvin);
 
-		RobotLocalizationViewer viewer = new RobotLocalizationViewer( localizer);
+		RobotLocalizationViewer viewer = new RobotLocalizationViewer(localizer);
 
 		/*
-		 * this thread controls the continuous update. If it is not started, 
-		 * you can only click through your localisation stepwise
+		 * this thread controls the continuous update. If it is not started, you can
+		 * only click through your localisation stepwise
 		 */
-		new LocalizationDriver( 500, viewer).start();
+		new LocalizationDriver(500, viewer).start();
 	}
-}	
+}
